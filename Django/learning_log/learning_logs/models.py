b43cbfcc65c4dc9a65c35c1de2edcc2a76ef3f8b
JRushFobos <fobos_media@mail.ru>
2022-07-24 15:17:45 +0500

    add Chapter_18_Django

--- Django/learning_log/learning_logs/models.py
+++ Django/learning_log/learning_logs/models.py
@@ -21,5 +21,7 @@ class Entry(models.Model):
     
     def __str__ (self):
         '''Возвращает строковое прдеставление модели'''
-        return f'{self.text[:50]}...'
-        
+        if len(self.text)>50:
+            return f'{self.text[:50]}...'
+        else:
+            return self.text
