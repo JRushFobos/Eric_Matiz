678355889c336d776e1d1b234f6c68fc302c16ad
JRushFobos <fobos_media@mail.ru>
2022-08-02 02:09:38 +0500

    update Django Snippets

--- Django/learning_log/learning_logs/urls.py
+++ Django/learning_log/learning_logs/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     #Домашняя страница
     path ('',views.index, name='index'),
     #Страница со списком всех тем
-    path ('topics/', views.topics,name='topics')
+    path ('topics/', views.topics, name='topics'),
+    #Страница с подробной информацией по каждой отдельной теме
+    path ('topics/<int:topic_id>', views.topic, name='topic')
 ]
